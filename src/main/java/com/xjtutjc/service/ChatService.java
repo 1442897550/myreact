31c728568e51ece5f6caa59fb8181a429ccf6671
JiachengTong <ahutjc18@163.com>
2026-03-31 16:13:44 +0800

    feature：fix steam wait

--- src/main/java/com/xjtutjc/service/ChatService.java
+++ src/main/java/com/xjtutjc/service/ChatService.java
@@ -25,57 +25,66 @@ public class ChatService {
     @Resource
     private ToolCallService toolCallService;
 
+    private Boolean isTool(GenerationResult result){
+        if(result == null) return false;
+        Message message = result.getOutput().getChoices().get(0).getMessage();
+        return !CollectionUtils.isEmpty(message.getToolCalls());
+    }
+
     public Flowable<GenerationResult> recurToolCallStream(Flowable<GenerationResult> flowable,ChatContext chatContext){
         //递归调用当前函数，判断是否要执行工具
         //拿到所有结果，合并后进行工具调用，将工具调用结果放入上下文，重新请求模型，再进行递归
-        return flowable.toList().flatMapPublisher(results -> {
-            Map<Integer, ToolCallContext> toolCallContextMap = new HashMap<>();
-            for (GenerationResult result : results) {
-                GenerationOutput output = result.getOutput();
-                GenerationOutput.Choice choice = output.getChoices().get(0);
-                Message message = choice.getMessage();
-                if(CollectionUtils.isEmpty(message.getToolCalls())){
-                    return flowable;
-                }
-                ToolCallBase toolCall = message.getToolCalls().get(0);
-                ToolCallFunction functionCall = (ToolCallFunction) toolCall;
-                ToolCallContext context = toolCallContextMap.getOrDefault(functionCall.getIndex(), new ToolCallContext());
-                if (!StringUtils.isEmpty(functionCall.getFunction().getName())) {
-                    context.setName(functionCall.getFunction().getName());
-                }
-                if (!StringUtils.isEmpty(functionCall.getId())) {
-                    context.setId(functionCall.getId());
-                }
-                if(StringUtils.isEmpty(context.getArgs())){
-                    context.setArgs(functionCall.getFunction().getArguments());
-                }else{
-                    context.setArgs(context.getArgs() + functionCall.getFunction().getArguments());
-                }
-                context.getToolCallBaseList().add(functionCall);
-                context.setIndex(functionCall.getIndex());
-                toolCallContextMap.put(functionCall.getIndex(),context);
-                System.out.println("工具添加" + functionCall.getId() + "工具参数" + functionCall.getFunction().getArguments() + "工具目标调用" + functionCall.getFunction().getName());
+        return flowable.firstElement().flatMapPublisher(generationResult -> {
+            if (isTool(generationResult)){
+                return flowable.toList().flatMapPublisher(results -> {
+                    Map<Integer, ToolCallContext> toolCallContextMap = new HashMap<>();
+                    for (GenerationResult result : results) {
+                        GenerationOutput output = result.getOutput();
+                        GenerationOutput.Choice choice = output.getChoices().get(0);
+                        Message message = choice.getMessage();
+                        ToolCallBase toolCall = message.getToolCalls().get(0);
+                        ToolCallFunction functionCall = (ToolCallFunction) toolCall;
+                        ToolCallContext context = toolCallContextMap.getOrDefault(functionCall.getIndex(), new ToolCallContext());
+                        if (!StringUtils.isEmpty(functionCall.getFunction().getName())) {
+                            context.setName(functionCall.getFunction().getName());
+                        }
+                        if (!StringUtils.isEmpty(functionCall.getId())) {
+                            context.setId(functionCall.getId());
+                        }
+                        if(StringUtils.isEmpty(context.getArgs())){
+                            context.setArgs(functionCall.getFunction().getArguments());
+                        }else{
+                            context.setArgs(context.getArgs() + functionCall.getFunction().getArguments());
+                        }
+                        context.getToolCallBaseList().add(functionCall);
+                        context.setIndex(functionCall.getIndex());
+                        toolCallContextMap.put(functionCall.getIndex(),context);
+                        System.out.println("工具添加" + functionCall.getId() + "工具参数" + functionCall.getFunction().getArguments() + "工具目标调用" + functionCall.getFunction().getName());
+                    }
+                    for (Integer index : toolCallContextMap.keySet()) {
+                        ToolCallContext context = toolCallContextMap.get(index);
+                        String result = toolCallService.getResult(toolCallContextMap.get(index));
+                        Message assistMessage = Message.builder()
+                                .toolCalls(context.getToolCallBaseList())
+                                .role(Role.ASSISTANT.getValue())
+                                .toolCallId(context.getId())
+                                .build();
+                        chatContext.getMessages().add(assistMessage);
+                        Message toolMessage = Message
+                                .builder()
+                                .toolCalls(context.getToolCallBaseList())
+                                .toolCallId(context.getId())
+                                .role(Role.TOOL.getValue())
+                                .content(result)
+                                .build();
+                        chatContext.getMessages().add(toolMessage);
+                    }
+                    Flowable<GenerationResult> generationResultFlowable = chatModel.streamChat(chatContext);
+                    return recurToolCallStream(generationResultFlowable,chatContext);
+                });
+            }else{
+                return flowable;
             }
-            for (Integer index : toolCallContextMap.keySet()) {
-                ToolCallContext context = toolCallContextMap.get(index);
-                String result = toolCallService.getResult(toolCallContextMap.get(index));
-                Message assistMessage = Message.builder()
-                        .toolCalls(context.getToolCallBaseList())
-                        .role(Role.ASSISTANT.getValue())
-                        .toolCallId(context.getId())
-                        .build();
-                chatContext.getMessages().add(assistMessage);
-                Message toolMessage = Message
-                        .builder()
-                        .toolCalls(context.getToolCallBaseList())
-                        .toolCallId(context.getId())
-                        .role(Role.TOOL.getValue())
-                        .content(result)
-                        .build();
-                chatContext.getMessages().add(toolMessage);
-            }
-            Flowable<GenerationResult> generationResultFlowable = chatModel.streamChat(chatContext);
-            return recurToolCallStream(generationResultFlowable,chatContext);
         });
     }
 }
